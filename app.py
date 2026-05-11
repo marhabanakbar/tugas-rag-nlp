c9eed76bd9a4e3b7143d804d0e63bc5ff59293cb
Arifian Saputra <arifiansaputra43@gmail.com>
2026-05-11 16:02:21 +0700

    Initial update

--- app.py
+++ app.py
@@ -28,9 +28,9 @@ USE_RERANKER = True
 # 5) Temperatur generasi 
 TEMPERATURE = 0.2
 # 6) Model yang tersedia di Groq ada banyak, coba ganti-ganti ke yang lain dan coba bandingkan hasilnya
-GROQ_MODEL = "llama-3.1-8b-instant"  # model default di Groq
+GROQ_MODEL = "openai/gpt-oss-120b"  # model default di Groq
 
-# Model lain yang bisa dicobain: gpt-oss-120b, llama-3.3-70b-versatile
+# Model lain yang bisa dicobain: openai/gpt-oss-120b, llama-3.3-70b-versatile
 # -----------------------------
 
 # (non-tunable tapi penting)
